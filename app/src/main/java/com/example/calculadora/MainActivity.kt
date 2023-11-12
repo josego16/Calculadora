43cd90fc3db57b206510efe44d259ffad72bf9d9
josego16 <jgomlin@g.educaand,es>
2023-11-12 23:18:11 +0100

    hemos terminado el diseño de la calculadora

--- app/src/main/java/com/example/calculadora/MainActivity.kt
+++ app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -1,9 +1,30 @@
 package com.example.calculadora
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var btnDel: Button
+    private lateinit var btnAc: Button
+    private lateinit var btnMasMenos: Button
+    private lateinit var btnPorcentaje: Button
+    private lateinit var btnDividir: Button
+    private lateinit var btnSiete: Button
+    private lateinit var btnOcho: Button
+    private lateinit var btnNueve: Button
+    private lateinit var btnMultiplicar: Button
+    private lateinit var btnCuatro: Button
+    private lateinit var btnCinco: Button
+    private lateinit var btnSeis: Button
+    private lateinit var btnRestar: Button
+    private lateinit var btnUno: Button
+    private lateinit var btnDos: Button
+    private lateinit var btnTres: Button
+    private lateinit var btnSumar: Button
+    private lateinit var btnCero: Button
+    private lateinit var btnComa: Button
+    private lateinit var btnIgual: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
